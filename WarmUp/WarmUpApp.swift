fded4454a3fd674bd14e699c3567911084a82e64
Rowoon <dev.rowoon@gmail.com>
2025-04-07 11:27:29 +0900

    Initial Commit

---
+++ WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by 이로운 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
