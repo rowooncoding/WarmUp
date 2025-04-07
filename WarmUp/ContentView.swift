5aaf4a5d1cdf16e36df041a27fbbf5a4380b8055
Rowoon <dev.rowoon@gmail.com>
2025-04-07 11:36:55 +0900

    가공해보기

--- WarmUp/ContentView.swift
+++ WarmUp/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
 //                .imageScale(.large)
 //                .foregroundStyle(.tint)
 //            Text("Hello, world!")
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
     }
