00c09a5eec993ddf53bc98ac04f9221b67c9f9d8
Rowoon <dev.rowoon@gmail.com>
2025-04-07 12:07:14 +0900

    그룹 잘못만듬ㅎ

---
+++ New Group/New Group/WarmUp/01/Layout.swift
@@ -0,0 +1,18 @@
+//
+//  Layout.swift
+//  WarmUp
+//
+//  Created by 이로운 on 4/7/25.
+//
+
+import SwiftUI
+
+struct Layout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Layout()
+}
