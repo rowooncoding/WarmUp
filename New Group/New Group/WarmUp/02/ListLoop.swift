fbb9a60e1cb29448a80d92064a9b94af0b6751e7
Rowoon <dev.rowoon@gmail.com>
2025-04-07 17:25:27 +0900

    리스트 추가 삭제 및 스테이트, 바인딩 공부

--- New Group/New Group/WarmUp/02/ListLoop.swift
+++ New Group/New Group/WarmUp/02/ListLoop.swift
@@ -16,7 +16,7 @@ struct Fruit: Hashable {
 struct ListLoop: View {
     let arr: [String] = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
     
-    var favoriteFruits = [
+    @State var favoriteFruits = [
         Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
         Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
         Fruit(name: "Cherry", matchFruitName: "DoubleKiwi", price: 4000),
@@ -24,18 +24,43 @@ struct ListLoop: View {
         Fruit(name: "ElderBerry", matchFruitName: "DoubleKiwi", price: 8000),
     ]
     
+    @State var fruitName: String = ""
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(favoriteFruits, id: \.self) { item in
-                    VStack(alignment: .leading) {
-                        Text("name : \(item.name)")
-                        Text("matchFruitName : \(item.matchFruitName)")
-                        Text("price : \(item.price)")
+            VStack {
+                
+                HStack {
+                    // 텍스트필드에 입력되면 $fruitName이 변하면서 동시에 위의 fruitName 변수도 변하길 바라기 때문에 바인딩
+                    TextField("insert fruit name", text: $fruitName)
+                    
+                    Button {
+                        // 이게 입력되면 새로운 배열을 추가해서 배열을 다시 보여줘야 하기 때문에 state
+                        favoriteFruits.append(Fruit(name: fruitName, matchFruitName: "Apple", price: 1000))
+                    } label: {
+                        Text("insert")
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+
+                }
+                .padding()
+                
+                List {
+                    ForEach(favoriteFruits, id: \.self) { item in
+                        VStack(alignment: .leading) {
+                            Text("name : \(item.name)")
+                            Text("matchFruitName : \(item.matchFruitName)")
+                            Text("price : \(item.price)")
+                        }
+                    }.onDelete { IndexSet in
+                        favoriteFruits.remove(atOffsets: IndexSet) // 기본 제공 기능 사용
                     }
                 }
+                .navigationTitle("Fruits List")
             }
-            .navigationTitle("Fruits List")
         }
     }
 }
