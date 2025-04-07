c398c259512e4b7a3ac976bc78fa55662054e3d8
Rowoon <dev.rowoon@gmail.com>
2025-04-07 16:59:35 +0900

    데이터 모델링해서 랜더링

--- New Group/New Group/WarmUp/02/ListLoop.swift
+++ New Group/New Group/WarmUp/02/ListLoop.swift
@@ -7,14 +7,32 @@
 
 import SwiftUI
 
+struct Fruit: Hashable {
+    let name: String
+    let matchFruitName: String
+    let price: Int
+}
+
 struct ListLoop: View {
     let arr: [String] = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+    
+    var favoriteFruits = [
+        Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
+        Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
+        Fruit(name: "Cherry", matchFruitName: "DoubleKiwi", price: 4000),
+        Fruit(name: "DoubleKiwi", matchFruitName: "ElderBerry", price: 2400),
+        Fruit(name: "ElderBerry", matchFruitName: "DoubleKiwi", price: 8000),
+    ]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(arr, id: \.self) { item in
-                    Text(item)
+                ForEach(favoriteFruits, id: \.self) { item in
+                    VStack(alignment: .leading) {
+                        Text("name : \(item.name)")
+                        Text("matchFruitName : \(item.matchFruitName)")
+                        Text("price : \(item.price)")
+                    }
                 }
             }
             .navigationTitle("Fruits List")
